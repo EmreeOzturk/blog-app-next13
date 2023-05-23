fbcb0be29f41a36b5ed05d02eec45bf22acbac97
Emre <emreeozturk42@gmail.com>
2023-05-23 17:47:54 +0300

    header updated

--- components/layout/header/Header.tsx
+++ components/layout/header/Header.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
-import styles from './Header.module.css'
+'use client';
+import { useState } from 'react';
+import Link from 'next/link';
+import styles from './Header.module.css';
+import Image from 'next/image';
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [showDropDown, setShowDropDown] = useState(false);
 
-export default Header
+  const handleShowDropDown = () => setShowDropDown(!showDropDown);
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <h2 className={styles.left}>
+          <Link href="/">Blog App</Link>
+        </h2>
+        <ul className={styles.right}>
+          {loggedIn ? (
+            <div>
+              <Image
+                onClick={handleShowDropDown}
+                src="/user.png"
+                width={30}
+                height={30}
+                alt="user"
+              />
+            </div>
+          ) : (
+            <li>
+              <Link href="/login">Login</Link>
+            </li>
+          )}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
