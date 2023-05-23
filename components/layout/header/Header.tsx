056c45b2f431edf22251e600785ab23716cda5f3
Emre <emreeozturk42@gmail.com>
2023-05-23 18:24:23 +0300

    header updated and static blog data added as hardcoded

--- components/layout/header/Header.tsx
+++ components/layout/header/Header.tsx
@@ -3,11 +3,14 @@ import { useState } from 'react';
 import Link from 'next/link';
 import styles from './Header.module.css';
 import Image from 'next/image';
+import person from '@/public/person.jpg';
+import { AiOutlineClose } from 'react-icons/ai';
 const Header = () => {
   const [loggedIn, setLoggedIn] = useState(false);
   const [showDropDown, setShowDropDown] = useState(false);
 
-  const handleShowDropDown = () => setShowDropDown(!showDropDown);
+  const handleShowDropDown = () => setShowDropDown((prev) => true);
+  const handleHideDropDown = () => setShowDropDown((prev) => false);
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
@@ -15,20 +18,42 @@ const Header = () => {
           <Link href="/">Blog App</Link>
         </h2>
         <ul className={styles.right}>
-          {loggedIn ? (
+          {!loggedIn ? (
             <div>
               <Image
                 onClick={handleShowDropDown}
-                src="/user.png"
-                width={30}
-                height={30}
-                alt="user"
+                src={person}
+                width={60}
+                height={60}
+                alt="person"
               />
+              {showDropDown && (
+                <div className={styles.dropdown}>
+                  <AiOutlineClose
+                    onClick={handleHideDropDown}
+                    className={styles.closeIcon}
+                  />
+                  <button
+                    onClick={handleHideDropDown}
+                    className={styles.logout}
+                  >
+                    Logout
+                  </button>
+                  <Link
+                    onClick={handleHideDropDown}
+                    href="/create-post"
+                    className={styles.create}
+                  >
+                    Create
+                  </Link>
+                </div>
+              )}
             </div>
           ) : (
-            <li>
-              <Link href="/login">Login</Link>
-            </li>
+            <>
+              <button className={styles.login}>Log in</button>
+              <Link href="/register">Register</Link>
+            </>
           )}
         </ul>
       </div>
