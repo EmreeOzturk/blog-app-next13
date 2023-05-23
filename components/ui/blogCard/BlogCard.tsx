e79f50338db11ce1c8b579ec35ac705edcbc0c4d
Emre <emreeozturk42@gmail.com>
2023-05-23 17:10:34 +0300

    components created

---
+++ components/ui/blogCard/BlogCard.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./BlogCard.module.css";
+const BlogCard = () => {
+  return <div>BlogCard</div>;
+};
+
+export default BlogCard;
---
+++ components/ui/comment/Comment.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './Comment.module.css'
+const Comment = () => {
+  return (
+    <div>Comment</div>
+  )
+}
+
+export default Comment
